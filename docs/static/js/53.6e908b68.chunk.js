27800ee2ab5da1a9e2e6266f9d653026decb2c62
CliffCrerar <cliff.crerar@gmail.com>
2020-08-29 14:39:04 +0200

    yes

---
+++ docs/static/js/53.6e908b68.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpprivacy-policies"]=this["webpackJsonpprivacy-policies"]||[]).push([[53],{208:function(n,t,i){"use strict";i.r(t),i.d(t,"startStatusTap",(function(){return o}));var e=i(5),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.j)((function(){var t=n.innerWidth,i=n.innerHeight,o=document.elementFromPoint(t/2,i/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(e.g)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.6e908b68.chunk.js.map
